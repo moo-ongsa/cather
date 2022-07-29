6caa19c4a7190e95920fdae0b1ef6ac44577d458
moo-ongsa <ongsa.sungkanit@gmail.com>
2022-07-29 18:08:08 +0700

    fix port

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     secure: true,
-    host: 'cather.herokuapp.com',
-    port: '443'
+    host: "/",
+    port: 443,
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
